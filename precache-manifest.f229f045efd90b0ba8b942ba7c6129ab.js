6307f565d99dc58f9da120c6504577504a51ed3f
Raj Patil <raj.patil20@pccoepune.org>
2023-02-01 20:46:04 +0530

    Updates

---
+++ precache-manifest.f229f045efd90b0ba8b942ba7c6129ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a35e98290648621b487a",
+    "url": "/NewsEra/static/css/main.fff623f1.chunk.css"
+  },
+  {
+    "revision": "a35e98290648621b487a",
+    "url": "/NewsEra/static/js/main.a35e9829.chunk.js"
+  },
+  {
+    "revision": "8a1229037873ab4e6605",
+    "url": "/NewsEra/static/js/1.8a122903.chunk.js"
+  },
+  {
+    "revision": "689aefc2ac7a97c7a1cb",
+    "url": "/NewsEra/static/js/2.689aefc2.chunk.js"
+  },
+  {
+    "revision": "f5ed66f3921dd2bc6fc4",
+    "url": "/NewsEra/static/js/runtime~main.f5ed66f3.js"
+  },
+  {
+    "revision": "db0d190da790c3a9c64e3689f580b247",
+    "url": "/NewsEra/static/media/loading.db0d190d.gif"
+  },
+  {
+    "revision": "25d27692a36f2ed5344c5b7e73485cb5",
+    "url": "/NewsEra/index.html"
+  }
+];
